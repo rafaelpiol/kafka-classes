b8a3b2c178c9d394b0e15065aeffb2984affad66
Rafael Piol <rafael.piol@gmail.com>
2023-10-02 22:44:46 -0300

    Aula 4.4

--- ecommerce/src/main/java/br/com/alura/ecommerce/KafkaService.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -15,10 +16,20 @@ class KafkaService implements Closeable {
 	private final ConsumerFunction parse;
 	
 	KafkaService(String groupId, String topic, ConsumerFunction parse) {
-		this.parse = parse;
-		this.consumer = new KafkaConsumer<>(properties(groupId));
+		this(parse, groupId);
 		consumer.subscribe(Collections.singletonList(topic));
 	}
+	
+	KafkaService(String groupId, Pattern topic, ConsumerFunction parse) {
+		this(parse, groupId);
+		consumer.subscribe(topic);
+	}
+
+	private KafkaService(ConsumerFunction parse, String groupId){
+		this.parse = parse;
+		this.consumer = new KafkaConsumer<>(properties(groupId));
+	}
+	
 	void run() {
 		while(true) {
 			var records = consumer.poll(Duration.ofMillis(1000));
